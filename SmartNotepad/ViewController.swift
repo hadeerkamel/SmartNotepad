b0254a770ea26a7c0302a26aa48abb06ebfa7dc8
Hadeer <Hadeer_kamel@outlook.com>
2021-06-18 19:34:18 +0200

    first commit

---
+++ SmartNotepad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartNotepad
+//
+//  Created by Hadeer Kamel on 6/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
